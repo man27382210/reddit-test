71e2d9b6883f22579325bc36b1f4060db830cba6
taiwei.tseng <taiwei.tseng@rakuten.com>
2018-06-08 17:08:09 +0900

    Add clinet based on react-waterfall

--- client/src/constants/memberLoginConstants.js
+++
@@ -1,5 +0,0 @@
-export const memberLoginConstants = {
-    SUCCESS: 'LOGIN_SUCCESS',
-    ERROR: 'LOGIN_ERROR',
-    CLEAR: 'LOGIN_CLEAR',
-}
--- client/src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-
-import mLReducer from 'memberLoginReducer'
-
-export default combineReducers({
-    mLReducer
-})
